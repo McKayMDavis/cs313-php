0b7940a3c5c656c3dc7a50105546fc1c0e83f13c
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 13:19:41 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -14,7 +14,7 @@
 	require 'header.php';
 	?>
 	<div class="container-fluid">
-		<p><i>Oh, sometimes I sit and think, and then again I just sit.</i></p>
+		<h3 class="text-center"><i>"Oh, sometimes I sit and think, and then again I just sit."</i></h3>
 		<div class="row">
 			<div class="col-sm-4">.col-sm-4</div>
 			<div class="col-sm-8">.col-sm-8</div>
