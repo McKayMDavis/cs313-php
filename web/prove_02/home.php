c1139abc5d71e9ae9d0c1d00ed851b58f6fcf7e0
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 13:35:38 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -20,9 +20,11 @@
 		<h4>History</h4>
 		<div class="row">
 			<div class="col-sm-8">
-				Sitting has been part of humanity since before humans were humans--just look at apes, they sit. These days, we might sit on a couch, chair, or something else raised. In ages past, we would have likely just sat on the ground, tree stumps, or rocks. In a way, every time you are outdoors and sit on a log or rock, you are gaining a deeper connection to our ancient predecessors.
+				Sitting has been part of humanity since before humans were humans--just look at apes, they sit. These days, we might sit on a couch, chair, or something else raised. In ages past, we would have likely just sat on the ground, tree stumps, or rocks. In a way, every time you are outdoors and sit on a log or rock, you are gaining a deeper connection to our ancient predecessors. Think back on a time when you have sat. Did it feel nostalgic? Now you know why.
 			</div>
-			<div class="col-sm-4">.col-sm-8</div>
+			<div class="col-sm-4">
+				<img src="images/sitting_caveman.jpg">
+			</div>
 		</div>
 	</div>
 </body>
