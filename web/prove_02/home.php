195389dc223d97aa532ef777a2443cc8601e513b
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 12:27:01 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -8,7 +8,7 @@
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body style="width: 100%; margin: 0">
 <div class="container-fluid">
 	<header style="background-color: lightgrey">
 		<h1>Homepage</h1>
