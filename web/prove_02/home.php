0156e757816a6879cc563d567718df1b59671c51
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 13:52:17 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -20,12 +20,12 @@
 		<div class="row">
 			<div class="col-sm-8">
 				<h4>History</h4>
-				Sitting has been part of humanity since before humans were humans--just look at apes, they sit. These days, we might sit on a couch, chair, or something else raised. In ages past, we would have likely just sat on the ground, tree stumps, or rocks. In a way, every time you are outdoors and sit on a log or rock, you are gaining a deeper connection to our ancient predecessors. Think back on a time when you have sat. Did it feel nostalgic? Now you know why.
+				<p>Sitting has been part of humanity since before humans were humans--just look at apes, they sit. These days, we might sit on a couch, chair, or something else raised. In ages past, we would have likely just sat on the ground, tree stumps, or rocks. In a way, every time you are outdoors and sit on a log or rock, you are gaining a deeper connection to our ancient predecessors. Think back on a time when you have sat. Did it feel nostalgic? Now you know why.</p>
 				<br/>
 				<h4>Why Sit?</h4>
-				Right now you are probably asking, "Well, that's some really neat history, but why should I even consider sitting?" If you aren't asking this question, you are probably already an avid sitter, in which case, please leave this page so we can save our limited server space for those that really need it.
+				<p>Right now you are probably asking, "Well, that's some really neat history, but why should I even consider sitting?" If you aren't asking this question, you are probably already an avid sitter, in which case, please leave this page so we can save our limited server space for those that really need it.</p>
 
-				The main reason you should consider sitting is that it is healthy. There are many foot and leg diseases associated with never sitting.
+				<p>The main reason you should consider sitting is that it is healthy. There are many foot and leg diseases associated with never sitting. I'll save you the gory details. Besides your health (honestly though, if you need another reason, you might want to see a doctor) sitting is comfortable and enjoyable. Every expert I have ever interviewed on the topic agrees that sitting is nice. If you don't believe me, go ask anyone who is anyone.</p>
 			</div>
 			<div class="col-sm-4">
 				<div class="media">
