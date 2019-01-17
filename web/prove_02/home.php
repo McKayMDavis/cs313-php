748f8be3724aa7807d1923a009f7559a0c0f78cf
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 12:15:24 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -10,9 +10,33 @@
 </head>
 <body>
 <div class="container-fluid">
-	<header><h1>Homepage</h1></header>
-</div>
-<div class="container">
+	<header>
+		<h1>Homepage</h1>
+	</header>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span> 
+				</button>
+				<a class="navbar-brand" href="#">WebSiteName</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Page 1</a></li>
+					<li><a href="#">Page 2</a></li> 
+					<li><a href="#">Page 3</a></li> 
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div class="row">
   		<div class="col-sm-4">.col-sm-4</div>
   		<div class="col-sm-8">.col-sm-8</div>
