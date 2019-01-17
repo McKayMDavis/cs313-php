aeb6d5469f47082b9f5eb02ba57f71fb00d39306
McKay Davis <mckaymdavis@gmail.com>
2019-01-17 14:16:05 -0700

    stuff

--- web/prove_02/home.php
+++ web/prove_02/home.php
@@ -22,7 +22,7 @@
 				<h4>History</h4>
 				<p>Sitting has been part of humanity since before humans were humans--just look at apes, they sit. These days, we might sit on a couch, chair, or stool (I bet that solved some unanswered questions about various objects found in your house). In ages past, we would have likely just sat on the ground, tree stumps, or rocks. In a way, every time you are outdoors and sit on a log or rock, you are gaining a deeper connection to our ancient predecessors. Think back on a time when you have sat. Did it feel nostalgic? Now you know why.</p>
 
-				<p>As sitting progressed through the ages...it largely remained the same, requiring the same form, muscles, and actions regardless of the item being sat upon. The typical sitting form is to place one's hind-quarters upon some object, balancing the majority of your bodyweight on this end, but using your feet as stabilizers. This is not the case always as you will find out if you follow our program explained in the conclusion.</p>
+				<p>As sitting progressed through the ages...it largely remained the same, requiring the same form, muscles, and actions regardless of the item being sat upon. The typical sitting form is to place one's hind-quarters upon some object, balancing the majority of your bodyweight on this end, but using your feet as stabilizers (you can see this form in the image to the right). This is not always the case as you will discover if you follow our program as explained in the conclusion.</p>
 			</div>
 			<div class="col-sm-4">
 				<div class="media">
@@ -41,7 +41,7 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<h4>Conclusion</h4>
-				<p>Go out there and practice sitting for yourself! You can see why it is beneficial and useful. If you need help learning how to sit, feel free to sign up for our lessons! One inexpensive lesson (only $500) will see you sitting in no time, then use our guided practice (an additional $300) to practice on your own!
+				<p>Go out there and practice sitting for yourself! You can see why it is beneficial and useful. If you need help learning how to sit, feel free to sign up for our lessons! One inexpensive lesson (only $500) will see you sitting in no time, then use our guided practice (an additional $300) to discover new skills on your own!
 			</div>
 		</div>
 	</div>
