52fe05aea6c2bc496af5d94a2b49c9644321ef84
McKay Davis <mckaymdavis@gmail.com>
2019-01-22 10:24:21 -0700

    stuff

---
+++ web/teach_03/results.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bob2</title>
+</head>
+<body>
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$major = $_POST["major"];
+$comments = $_POST["comments"];
+?>
+
+Name: <?php echo $name;?>
+<br>
+Email: <a href= "'"<?php echo "mailto:" . $email;?>"'" > <?php echo $email; ?> </a>
+<br>
+Major: <?php echo $major;?>
+<br>
+Comments: <?php echo $comments;?>
+</body>
+</html>
