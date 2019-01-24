6d4d572520d12edcb088705b40fdbebd239247c9
McKay Davis <mckaymdavis@gmail.com>
2019-01-24 13:09:12 -0700

    stuff

---
+++ web/prove_03/view_cart.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Browse Items</title>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="custom.css">
+</head>
+<body>
+	<header><h1>Shopping</h1></header>
+	<div id="nav">
+	<?php
+	require('./header.php');
+	?>
+	</div>
+	
+</body>
+</html>
