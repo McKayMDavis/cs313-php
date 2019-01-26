b141b3e7e834e8edd91d74a89da44f8021b95e19
McKay Davis <mckaymdavis@gmail.com>
2019-01-26 16:01:03 -0700

    stuff

--- web/prove_03/add_item.php
+++ web/prove_03/add_item.php
@@ -10,7 +10,8 @@ $price = preg_split("/-/", $input)[1];
 //Just so you know, this forces the user to only be able to purchase one of each item
 //We'll want to make this a dictionary with price and quantity added so that we can display these in the cart and checkout
 if (strpos($item, "Remove") === false) {
-	$_SESSION['items'][$item] = $item;
+	$_SESSION['items'][$item]['item'] = $item;
+	$_SESSION['items'][$item]['price'] = $price;
 } else {
 	$item = str_replace("Remove ", "", $item);
 	unset($_SESSION['items'][$item]);
