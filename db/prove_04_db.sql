2da2d9a517597a63ba08bf29d49fe6ad1a1651e4
McKay Davis <mckaymdavis@gmail.com>
2019-01-31 11:11:50 -0700

    add sql for prove 04

---
+++ db/prove_04_db.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS users, goal, total, expense, revenue CASCADE;
+
+CREATE TABLE users (
+  user_id        SERIAL       NOT NULL PRIMARY KEY
+, username       VARCHAR(100) NOT NULL UNIQUE
+, password       VARCHAR(100) NOT NULL
+, user_type      VARCHAR(20)  NOT NULL
+, date_entered   DATE         NOT NULL
+, last_update    INT          NOT NULL REFERENCES users(user_id)
+);
+
+CREATE TABLE goal (
+  goal_id        SERIAL       NOT NULL PRIMARY KEY
+, goal_expense   INT          NOT NULL
+, goal_revenue   INT          NOT NULL
+, goal_profits   INT          NOT NULL
+, year           SMALLINT     NOT NULL
+, date_entered   DATE         NOT NULL
+, last_update    INT          NOT NULL REFERENCES users(user_id)
+);
+
+CREATE TABLE total (
+  total_id       SERIAL       NOT NULL PRIMARY KEY
+, total_expense  INT          NOT NULL
+, total_revenue  INT          NOT NULL
+, total_profits  INT          NOT NULL
+, year           SMALLINT     NOT NULL
+, date_entered   DATE         NOT NULL
+, last_update    INT          NOT NULL REFERENCES users(user_id)
+, goal_id        INT          NOT NULL REFERENCES goal(goal_id) UNIQUE
+);
+
+CREATE TABLE expense (
+  expense_id     SERIAL       NOT NULL PRIMARY KEY
+, description    TEXT         NOT NULL
+, vendor         VARCHAR(100) NOT NULL
+, amount         INT          NOT NULL
+, date_entered   DATE         NOT NULL
+, last_update    INT          NOT NULL REFERENCES users(user_id)
+, total_id       INT          NOT NULL REFERENCES total(total_id)
+, goal_id        INT          NOT NULL REFERENCES goal(goal_id)
+);
+
+CREATE TABLE revenue (
+  revenue_id     SERIAL       NOT NULL PRIMARY KEY
+, description    TEXT         NOT NULL
+, client         VARCHAR(100) NOT NULL
+, amount         INT          NOT NULL
+, date_entered   DATE         NOT NULL
+, last_update    INT          NOT NULL REFERENCES users(user_id)
+, total_id       INT          NOT NULL REFERENCES total(total_id)
+, goal_id        INT          NOT NULL REFERENCES goal(goal_id)
+);
