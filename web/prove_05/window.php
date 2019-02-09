689eca43dd16cdddb74f095bb5eec1fc248d99a7
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 16:15:32 -0700

    prove_05

--- web/prove_05/window.php
+++ web/prove_05/window.php
@@ -3,8 +3,8 @@ session_start();
 $data = $_SESSION["query-results"];
 
 foreach ($data as $key => $rowname) {
-	foreach ($value as $colname => $data) {
-		echo $data;
+	foreach ($data[$key] as $colname => $item) {
+		echo $item;
 	}
 }
 ?>
