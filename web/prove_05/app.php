4b394441dd3de93b7f611624abe33ddfb534c40a
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 15:53:57 -0700

    prove_05

--- web/prove_05/app.php
+++ web/prove_05/app.php
@@ -55,6 +55,15 @@ session_start();
 	            data: frm.serialize(),
 	            success: function (data) {
 	            	console.log("Success 1")
+
+	                $.ajax({
+				    	url: 'window.php'
+				    	success: function(response) {
+				    		console.log("Success 2");
+				    		$("#plot-window").html(response);
+				    	}
+				    });
+				    
 	            },
 	            error: function (data) {
 	                console.log('An error occurred.');
