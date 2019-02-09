80d14fab6213adfa0bdc7e498c082525b4f31f42
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 16:36:27 -0700

    prove_05

--- web/prove_05/window.php
+++ web/prove_05/window.php
@@ -1,10 +1,31 @@
 <?php
 session_start();
 $data = $_SESSION["query-results"];
+$headers = array();
+
+echo "<table><tr>";
+foreach($data[0] as $colname => $datum) {
+	echo "<th>" . $colname . "</th>";
+}
+echo "</tr>";
 
 foreach ($data as $row) {
 	foreach ($row as $colname => $datum) {
-		echo $datum;
+		$headers[] = $colname;
 	}
 }
+
+
+
+/*// create header array here...$myHeaders
+// create data array here... $myData
+
+$csvName = 'temp.csv';
+
+$fp = fopen($csvName, 'w');
+fputcsv($fp, $myHeaders);
+foreach ($myData as $line) {
+    fputcsv($fp, $line);
+}
+fclose($fp);*/
 ?>
