5d1ca3d6ca6bfc47f74d20ba1333bd8cf60fcf57
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 15:00:39 -0700

    prove_05

--- web/prove_05/app.php
+++ web/prove_05/app.php
@@ -36,43 +36,7 @@
 			</form>
 		</div>
 		<div id="plot-window" class="col-sm-8">
-			<?php
-			session_start();
-			try
-			{
-				$dbUrl = getenv('DATABASE_URL');
 
-				$dbOpts = parse_url($dbUrl);
-
-				$dbHost = $dbOpts["host"];
-				$dbPort = $dbOpts["port"];
-				$dbUser = $dbOpts["user"];
-				$dbPassword = $dbOpts["pass"];
-				$dbName = ltrim($dbOpts["path"],'/');
-
-				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-			catch (PDOException $ex)
-			{
-				echo 'Error!: ' . $ex->getMessage();
-				die();
-			}
-
-			$table = 'expense';
-			$year = '2018';
-
-			$query = $db->prepare('SELECT * FROM :table WHERE year=:year');
-			$query->execute(array(':table' => $table, ':year' => $year));
-			$results = $query->fetchAll(PDO::FETCH_ASSOC);
-
-			$_SESSION["query-results"] = $results;
-
-			$data = $_SESSION["query-results"];
-
-			echo $data;
-			?>
 		</div>
 	</div>
 
