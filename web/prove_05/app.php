352bd8c0ccc4d9c1c697f628e5b2327084b273b4
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 15:06:12 -0700

    prove_05

--- web/prove_05/app.php
+++ web/prove_05/app.php
@@ -55,6 +55,7 @@ session_start();
 				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				echo 'Success';
 			}
 			catch (PDOException $ex)
 			{
