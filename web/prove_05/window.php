c60bfedb4c71ac8c16bff95736c3be6ff3adeaba
McKay Davis <mckaymdavis@gmail.com>
2019-02-09 19:28:48 -0700

    stuff

--- web/prove_05/window.php
+++ web/prove_05/window.php
@@ -33,8 +33,6 @@ fclose($fp);
 
 //make a plot from the csv (this doesn't work for some reason). The R script is called and executes but the image file doesn't seem to be saving.
 exec("Rscript plots.R", $response);
-$str = $response[0];
-$myobj = json_decode($str);
-echo json_encode($myobj);
+echo $response[0];
 echo "<img src='temp.png' alt='Plot Image'></img>";
 ?>
