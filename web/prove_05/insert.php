180efb02cd42e37cb6d505101542fadba96856bb
McKay Davis <mckaymdavis@gmail.com>
2019-02-12 16:48:56 -0700

    stuff

--- web/prove_05/insert.php
+++ web/prove_05/insert.php
@@ -10,8 +10,12 @@ $data = htmlspecialchars($_POST["data"]);
 //CHANGE LAST UPDATE TO BE THE USER THAT IS LOGGED IN
 if ($tname == 'expense') {
 	for ($i; $i < $nrow; $i++) {
-		$db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
-		$db->execute(array(':description' => $data[$i][1] , ':vendor' => $data[$i][2] , ':amount' => $data[$i][3] , ':year' => $data[$i][4]));
+		try {
+			$db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
+			$db->execute(array(':description' => $data[$i][1] , ':vendor' => $data[$i][2] , ':amount' => $data[$i][3] , ':year' => $data[$i][4]));
+		} catch (PDOException $ex) {
+			echo "FAILED" . $ex->getMessage();
+		}
 	}
 } elseif ($tname == 'revenue') {
 	for ($i; $i < $nrow; $i++) {
