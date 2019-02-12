36ef037ad85e431dfb2f82ba8f0af2e928859bc9
McKay Davis <mckaymdavis@gmail.com>
2019-02-12 15:38:27 -0700

    stuff

--- web/prove_05/construct_input.php
+++ web/prove_05/construct_input.php
@@ -3,15 +3,14 @@ require('db.php');
 $table = htmlspecialchars($_POST["data-type"]);
 
 if ($table == 'expense') {
-	$query = $db->query('SELECT * FROM expense WHERE false');
+	$query = $db->query('SELECT * FROM expense WHERE expense_id=1');
 } elseif ($table == 'revenue') {
-	$query = $db->query('SELECT * FROM revenue WHERE false');
+	$query = $db->query('SELECT * FROM revenue WHERE revenue_id=1');
 } elseif ($table == 'goal') {
-	$query = $db->query('SELECT * FROM goal WHERE false');
+	$query = $db->query('SELECT * FROM goal WHERE goal_id=1');
 }
 
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
-var_dump($results);
 
 $_SESSION["table-results"] = $results;
 $_SESSION["table-nrow"] = htmlspecialchars($_POST["nrow"]);
