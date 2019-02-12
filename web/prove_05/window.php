364e53f64bc9e028b9dd9fe828c2a4befe4d8f39
McKay Davis <mckaymdavis@gmail.com>
2019-02-12 12:40:47 -0700

    stuff

--- web/prove_05/window.php
+++ web/prove_05/window.php
@@ -34,7 +34,7 @@ fclose($fp);
 echo "<a href='temp.csv'>Download CSV</a><br>";
 
 //Had to use R buildpack to get Rscript installed in the slug
-exec("/app/bin/Rscript plots.R", $output);
+exec("/app/bin/Rscript /app/web/prove_05/plots.R", $output);
 var_dump($output);
 echo "<img src='temp.png' alt='Plot Image'></img>";
 ?>
