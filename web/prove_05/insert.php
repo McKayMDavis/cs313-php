f0a916a4899cc1d6f511081206dc41490381b50f
McKay Davis <mckaymdavis@gmail.com>
2019-02-13 14:51:16 -0700

    stuff

--- web/prove_05/insert.php
+++ web/prove_05/insert.php
@@ -9,6 +9,7 @@ $data = $_POST["data"];
 //CHANGE LAST UPDATE TO BE THE USER THAT IS LOGGED IN
 if ($tname == 'expense') {
 	for ($i = 0; $i < $nrow; $i++) {
+		var_dump($data[$i][3]);
 		$statement = $db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
 		$success = $statement->execute(array(':description' => $data[$i][0], ':vendor' => $data[$i][1], ':amount' => $data[$i][2], ':year' => $data[$i][3]));
 	}
