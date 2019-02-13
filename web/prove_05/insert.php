368313c3bbac8fccbcd00bd88a223185fedb1a0a
McKay Davis <mckaymdavis@gmail.com>
2019-02-12 22:03:31 -0700

    stuff

--- web/prove_05/insert.php
+++ web/prove_05/insert.php
@@ -9,13 +9,12 @@ var_dump($data);
 
 //CHANGE LAST UPDATE TO BE THE USER THAT IS LOGGED IN
 if ($tname == 'expense') {
-	for ($i; $i < $nrow; $i++) {
-		var_dump($i);
+	for ($i = 0; $i < $nrow; $i++) {
 		$db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
 		$db->execute(array(':description' => $data[$i][1], ':vendor' => $data[$i][2], ':amount' => $data[$i][3], ':year' => $data[$i][4]));
 	}
 } elseif ($tname == 'revenue') {
-	for ($i; $i < $nrow; $i++) {
+	for ($i = 0; $i < $nrow; $i++) {
 		$db->prepare('INSERT INTO expense (description, client, amount, year, date_entered, last_update, goal_id) VALUES (:description, :client, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
 		$db->execute(array(':description' => $data[$i][1], ':client' => $data[$i][2], ':amount' => $data[$i][3], ':year' => $data[$i][4]));
 	}
