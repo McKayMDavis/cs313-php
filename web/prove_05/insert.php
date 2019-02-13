d59d2b0537c1963a56188dcf97027247745797b3
McKay Davis <mckaymdavis@gmail.com>
2019-02-12 22:07:26 -0700

    stuff

--- web/prove_05/insert.php
+++ web/prove_05/insert.php
@@ -10,13 +10,14 @@ var_dump($data);
 //CHANGE LAST UPDATE TO BE THE USER THAT IS LOGGED IN
 if ($tname == 'expense') {
 	for ($i = 0; $i < $nrow; $i++) {
+		var_dump($data[$i][1])
 		$db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
-		$db->execute(array(':description' => $data[$i][1], ':vendor' => $data[$i][2], ':amount' => $data[$i][3], ':year' => $data[$i][4]));
+		$db->execute(array(':description' => $data[$i][0], ':vendor' => $data[$i][1], ':amount' => $data[$i][2], ':year' => $data[$i][3]));
 	}
 } elseif ($tname == 'revenue') {
 	for ($i = 0; $i < $nrow; $i++) {
 		$db->prepare('INSERT INTO expense (description, client, amount, year, date_entered, last_update, goal_id) VALUES (:description, :client, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
-		$db->execute(array(':description' => $data[$i][1], ':client' => $data[$i][2], ':amount' => $data[$i][3], ':year' => $data[$i][4]));
+		$db->execute(array(':description' => $data[$i][0], ':client' => $data[$i][1], ':amount' => $data[$i][2], ':year' => $data[$i][3]));
 	}
 }
 ?>
