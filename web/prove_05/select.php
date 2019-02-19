8b8b0578caa8ba20142e95b142d7392b4a94b27a
McKay Davis <mckaymdavis@gmail.com>
2019-02-19 14:46:45 -0700

    stuff

--- web/prove_05/select.php
+++ web/prove_05/select.php
@@ -4,11 +4,11 @@ $table = htmlspecialchars($_POST["data-type"]);
 $year = htmlspecialchars($_POST["year"]);
 
 if ($table == 'expense') {
-	$query = $db->prepare('SELECT * FROM expense WHERE year=:year');
+	$query = $db->prepare('SELECT expense_id, description, vendor, amount, year, date_entered, last_update FROM expense WHERE year=:year');
 } elseif ($table == 'revenue') {
-	$query = $db->prepare('SELECT * FROM revenue WHERE year=:year');
+	$query = $db->prepare('SELECT revenue_id, description, client, amount, year, date_entered, last_update FROM revenue WHERE year=:year');
 } elseif ($table == 'goal') {
-	$query = $db->prepare('SELECT * FROM goal WHERE year=:year');
+	$query = $db->prepare('SELECT goal_id, goal_expense, goal_revenue, goal_profits, year, date_entered, last_update FROM goal WHERE year=:year');
 }
 
 $query->execute(array(':year' => $year));
