4b0d66438a9b5b2eafd9c6f9426b09d151826d70
McKay Davis <mckaymdavis@gmail.com>
2019-02-19 12:57:34 -0700

    stuff

--- web/prove_05/register.php
+++ web/prove_05/register.php
@@ -22,7 +22,7 @@ if ($_SESSION["logged_in"] != 1) {
 	<?php require("header.php")?>
 	<div class="row">
 		<div class="container-fluid">
-			<h2 class="text-center">Welcome! Create an Account:</h2>
+			<h2 class="text-center">Register a New User:</h2>
 		</div>
 	</div>
 	<div class="row">
@@ -31,16 +31,35 @@ if ($_SESSION["logged_in"] != 1) {
 			<form id="login" action="register_db.php" method="POST">
 				<div class="form-group">
 					<label for="username">Username:</label>
-					<input type="text" class="form-control" id="username" placeholder="Enter username" name="username" autocomplete="off">
+					<input type="text" class="form-control" id="username" placeholder="Enter username" name="username" autocomplete="off" required="required">
 				</div>
 				<div class="form-group">
 					<label for="password">Password:</label>
-					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required="required">
+				</div>
+				<div class="form-group">
+					<label for="password-conf">Re-Enter Password:</label>
+					<input type="password" class="form-control" id="password-conf" placeholder="Enter password" name="password-conf" required="required">
 				</div>
 				<input type="submit" value="Login">
 			</form>
 		</div>
 		<div class="col-sm-4"></div>
 	</div>
+	<script type="text/javascript">
+		var password = document.getElementById("password")
+		, confirm_password = document.getElementById("password-conf");
+
+		function validatePassword(){
+			if(password.value != confirm_password.value) {
+				confirm_password.setCustomValidity("Passwords Don't Match");
+			} else {
+				confirm_password.setCustomValidity('');
+			}
+		}
+
+		password.onchange = validatePassword;
+		confirm_password.onkeyup = validatePassword;
+	</script>
 </body>
 </html>
