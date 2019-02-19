c649bf976583b2f245cd23ac50a2b72fe8251d2d
McKay Davis <mckaymdavis@gmail.com>
2019-02-19 13:28:01 -0700

    stuff

--- web/prove_05/register_db.php
+++ web/prove_05/register_db.php
@@ -9,10 +9,11 @@ if ($user_type == "Admin") {
 } elseif ($user_type == "User") {
 	$user_type_id = 2;
 }
+$last_update = $_SESSION["logged_in"]["id"];
 
 $statement1 = 
-$db->prepare("INSERT INTO users(username, password, user_type, date_entered, last_update) VALUES (:username, :password, :user_type, CURRENT_DATE, 1)");
-$statement1->execute(array(":username"=>$username, ":password"=>$password, ":user_type"=>$user_type_id));
+$db->prepare("INSERT INTO users(username, password, user_type, date_entered, last_update) VALUES (:username, :password, :user_type, CURRENT_DATE, :last_update)");
+$statement1->execute(array(":username"=>$username, ":password"=>$password, ":user_type"=>$user_type_id, ":last_update"=>$last_update));
 
 flush();
 header("Location: register.php");
