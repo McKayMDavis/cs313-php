e6836abb7e68edab0f3c2ec410cc562fd75f5a94
McKay Davis <mckaymdavis@gmail.com>
2019-02-19 11:53:35 -0700

    stuff

--- web/prove_05/login_db.php
+++ web/prove_05/login_db.php
@@ -10,17 +10,15 @@ $statement1 = $db->prepare("SELECT username FROM users WHERE username=:username"
 $statement1->execute(array(":username"=>$username));
 $results1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
 
-$statement2 = $db->prepare("SELECT password FROM users WHERE password=:password");
-$statement2->execute(array(":password"=>$password));
+$statement2 = $db->prepare("SELECT password FROM users WHERE username=:username");
+$statement2->execute(array(":username"=>$username));
 $results2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
-
-echo $results2[0]['password'];
 
 if (password_verify($password, $results2[0]['password'])) {
 	$logged_in = True;
 }
 
-/*$_SESSION["logged_in"] = $logged_in;
+$_SESSION["logged_in"] = $logged_in;
 if ($logged_in) {
 	flush();
 	header("Location: app.php");
@@ -29,5 +27,5 @@ if ($logged_in) {
 	flush();
 	header("Location: login.php");
 	die();
-}*/
+}
 ?>
