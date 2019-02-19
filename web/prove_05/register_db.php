a28f867be404b6ef609d863c8b9107242e16ca92
McKay Davis <mckaymdavis@gmail.com>
2019-02-19 11:05:05 -0700

    stuff

--- web/prove_05/register_db.php
+++ web/prove_05/register_db.php
@@ -6,7 +6,8 @@ $logged_in = False;
 $username = htmlspecialchars($_POST["username"]);
 $password = password_hash(htmlspecialchars($_POST["password"]), PASSWORD_DEFAULT);
 
-$statement1 = $db->prepare("INSERT INTO users(username, password, user_type, date_entered, last_update) VALUES (:username, :password, 1, CURRENT_DATE, 1)";
+$statement1 = 
+$db->prepare("INSERT INTO users(username, password, user_type, date_entered, last_update) VALUES (:username, :password, 1, CURRENT_DATE, 1)");
 $statement1->execute(array(":username"=>$username, ":password"=>$password));
 
 $_SESSION["logged_in"] = True;
