4c09e337caee6b097c08f4075b9e884a1a28e9d2
McKay Davis <mckaymdavis@gmail.com>
2019-02-21 09:59:44 -0700

    stuff

--- web/prove_05/insert.php
+++ web/prove_05/insert.php
@@ -5,13 +5,14 @@ session_start();
 $nrow = $_SESSION["table-nrow"];
 $tname = $_SESSION["table-name"];
 $data = $_POST["data"];
+$last_update = $_SESSION["logged_in"]["id"];
 
 //CHANGE LAST UPDATE TO BE THE USER THAT IS LOGGED IN
 if ($tname == 'expense') {
 	for ($i = 0; $i < $nrow; $i++) {
-		$statement = $db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
+		$statement = $db->prepare('INSERT INTO expense (description, vendor, amount, year, date_entered, last_update, goal_id) VALUES (:description, :vendor, :amount, :year, CURRENT_DATE, :last_update, (SELECT goal_id FROM goal WHERE year=:year))');
 		try {
-			$statement->execute(array(':description' => htmlspecialchars($data[$i][0]), ':vendor' => htmlspecialchars($data[$i][1]), ':amount' => htmlspecialchars($data[$i][2]), ':year' => htmlspecialchars($data[$i][3])));
+			$statement->execute(array(':description' => htmlspecialchars($data[$i][0]), ':vendor' => htmlspecialchars($data[$i][1]), ':amount' => htmlspecialchars($data[$i][2]), ':year' => htmlspecialchars($data[$i][3]), ':last_update'=>$last_update));
 		} catch (Exception $e) {
 			flush();
 			header("Location: enter.php?success=F");
@@ -21,9 +22,9 @@ if ($tname == 'expense') {
 	}
 } elseif ($tname == 'revenue') {
 	for ($i = 0; $i < $nrow; $i++) {
-		$statement = $db->prepare('INSERT INTO revenue (description, client, amount, year, date_entered, last_update, goal_id) VALUES (:description, :client, :amount, :year, CURRENT_DATE, 1, (SELECT goal_id FROM goal WHERE year=:year))');
+		$statement = $db->prepare('INSERT INTO revenue (description, client, amount, year, date_entered, last_update, goal_id) VALUES (:description, :client, :amount, :year, CURRENT_DATE, :last_update, (SELECT goal_id FROM goal WHERE year=:year))');
 		try {
-			$statement->execute(array(':description' => htmlspecialchars($data[$i][0]), ':client' => htmlspecialchars($data[$i][1]), ':amount' => htmlspecialchars($data[$i][2]), ':year' => htmlspecialchars($data[$i][3])));
+			$statement->execute(array(':description' => htmlspecialchars($data[$i][0]), ':client' => htmlspecialchars($data[$i][1]), ':amount' => htmlspecialchars($data[$i][2]), ':year' => htmlspecialchars($data[$i][3]), ':last_update'=>$last_update));
 		} catch (Exception $e) {
 			flush();
 			header("Location: enter.php?success=F");
