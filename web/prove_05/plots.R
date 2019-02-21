d0bada3649184e904c0664289e2197c5df5601c6
McKay Davis <mckaymdavis@gmail.com>
2019-02-21 11:05:34 -0700

    stuff

--- web/prove_05/plots.R
+++ web/prove_05/plots.R
@@ -4,8 +4,8 @@ data <- read.csv("/app/web/prove_05/temp.csv")
 library(tidyverse)
 p <- data %>% 
   ggplot(aes(x = date_entered, y = amount)) +
-  geom_point() +
-  theme_minimal()
+  geom_boxplot() +
+  theme_bw()
 ggsave("temp.png", p, "png", "/app/web/prove_05/")
 
 # png(filename = "/app/web/prove_05/temp.png", width = 500, height = 500)
