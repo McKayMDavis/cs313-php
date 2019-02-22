3f06d0628f90a6bc1337dfffb0bd912315eb6d30
McKay Davis <mckaymdavis@gmail.com>
2019-02-22 09:31:10 -0700

    stuff

--- web/prove_05/plots.R
+++ web/prove_05/plots.R
@@ -1,21 +1,23 @@
 args <- commandArgs(TRUE)
 data <- read.csv("/app/web/prove_05/temp.csv")
 
-# library(tidyverse)
-# p <- data %>% 
-#   ggplot(aes(x = date_entered, y = amount, fill = date_entered)) +
-#   geom_boxplot() +
-#   theme_bw()
-# 
-# 
-# tryCatch(ggsave("temp.png", p, "png", "/app/web/prove_05/", height = 6, width = 10)
-# , warning = function(w) {
-#   cat(w)
-# }, error = function(e) {
-#   cat(e)
-# })
+unlink("/app/web/prove_05/temp.png")
+
+library(ggplot2)
+p <- data %>%
+  ggplot(aes(x = date_entered, y = amount, fill = date_entered)) +
+  geom_boxplot() +
+  theme_bw()
 
 
-png(filename = "/app/web/prove_05/temp.png", width = 500, height = 500)
-plot(data$date_entered, data$amount)
-dev.off()
+tryCatch(ggsave("temp.png", p, "png", "/app/web/prove_05/", height = 6, width = 10)
+, warning = function(w) {
+  cat(w)
+}, error = function(e) {
+  cat(e)
+})
+
+
+# png(filename = "/app/web/prove_05/temp.png", width = 500, height = 500)
+# plot(data$date_entered, data$amount)
+# dev.off()
