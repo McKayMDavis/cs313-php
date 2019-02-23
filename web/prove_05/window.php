b622c67a192115efbeb15bd21ffbb0dd25a66148
McKay Davis <mckaymdavis@gmail.com>
2019-02-22 22:00:25 -0700

    stuff

--- web/prove_05/window.php
+++ web/prove_05/window.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if (!isset($_SESSION["logged-in"])) {
+	flush();
+	header("Location: login.php");
+	die();
+}
 if (!isset($_SESSION["plot-iter"])) {
 	$_SESSION["plot-iter"] = 1;
 } else {
